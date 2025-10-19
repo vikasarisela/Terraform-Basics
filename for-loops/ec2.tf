56eabd95fac370d656a2c7787a0372c5a62af722
unknown <vikas.arisela@gmail.com>
2025-10-19 11:37:17 +0400

    terraform basics

---
+++ for-loops/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "my_ec2_instance" {
+  for_each = var.environment
+  ami           = "ami-09c813fb71547fc4f" # Replace with a valid AMI ID for your region
+  instance_type = each.value
+  vpc_security_group_ids = [aws_security_group.allow-all.id]
+  tags = {
+    Name = each.key
+  }
+  # Optional: User data to run a script on instance launch
+  # user_data = file("install_script.sh")
+}
+
+resource "aws_security_group" "allow-all" {
+  name   = "allow-all"
+  
+  egress {
+    from_port        = 0   # all ports 
+    to_port          = 0    # all ports 
+    protocol         = "-1"   # -1 all protocols 
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  ingress {
+    from_port        = 0   # all ports 
+    to_port          = 0    # all ports 
+    protocol         = "-1"   # -1 all protocols 
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "allowing all in tags"
+  }
+
+}
